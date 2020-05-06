dae21124bbe1806d722a5c1a104f0c960f57fb41
Justin <justindoane.com>
2020-05-06 00:39:13 -0400

    First commit

---
+++ src/Server/Services/NameService.lua
@@ -0,0 +1,20 @@
+-- Name Service
+-- Username
+-- May 5, 2020
+
+
+
+local NameService = {Client = {}}
+
+
+function NameService:Start()
+	
+end
+
+
+function NameService:Init()
+	
+end
+
+
+return NameService
---
+++ src/Server/Services/TestService.lua
@@ -0,0 +1,20 @@
+-- Test Service
+-- Username
+-- May 6, 2020
+
+
+
+local TestService = {Client = {}}
+
+
+function TestService:Start()
+	
+end
+
+
+function TestService:Init()
+	
+end
+
+
+return TestService
